ce6b8f06bbc2e32905095b7fd3df971b8e644471
Egor <129444652+Alexandrovich99Egor@users.noreply.github.com>
2024-05-25 18:40:43 +0300

    Options/SQL

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["src/**/*.{html,js}", "back/home.php"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
